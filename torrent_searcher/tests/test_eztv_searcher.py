b667ab91f57c25f9753f41f7a490757adb843fa8
federulli <fede.rulli@gmail.com>
2020-06-20 16:18:43 -0400

    fix test

--- torrent_searcher/tests/test_eztv_searcher.py
+++ torrent_searcher/tests/test_eztv_searcher.py
@@ -3,7 +3,7 @@ import responses
 import json
 import requests
 from torrent_searcher.searchers.eztv_searcher import EztvSearcher, Torrent
-from data.eztv_responses import PAGE_1
+from torrent_searcher.tests.data.eztv_responses import PAGE_1
 
 URL = 'https://eztv.ag/api/get-torrents?imdb_id=3107288&limit=100&page={}'
 
